5cf7ec6d7713a2fcd6b5fd04e044f39089dfae68
samSAM <samuelbandola@gmail.com>
2022-05-29 01:56:53 +0800

    download nad filter full schedule with pagination

--- resources/views/schedules/view.blade.php
+++ resources/views/schedules/view.blade.php
@@ -19,59 +19,60 @@ if (! function_exists('removeParam')) {
     <div class="row">
         <main class="py-4">
 
-            <h3><i class="fa fa-info-circle"></i> Wpc2021 OCBS Schedule</h3>
 
-            <h5>May 2, 2022</h5>
 
             <div class="card card-info">
 
                 <div class="card-header">
-                    <h3 class="card-title"><i class="fa fa-cog"></i> Schedule Management</h3>
-                    <a class="btn btn-secondary float-right" href="{{ removeParam(request()->fullUrlWithQuery(['download' => '1']), 'downloadcurrent') }}">Download Excel</a>
+                    <h3 class="card-title"><i class="fa fa-cog"></i> Schedule</h3>
+                    <a class="btn btn-success float-right" href="{{ removeParam(request()->fullUrlWithQuery(['download' => '1']), 'downloadcurrent') }}">Download Excel</a>
                 </div>
 
                 <div class="card-body">
 
-                    <form class="form-horizontal">
+                    <form class="form-horizontal" method="get">
                         <div class="form-group row">
                         <div class="col-md-3">
-                            <input type="text" class="filter-group form-control" placeholder="Group Name / Code">
+                            <input type="text" class="filter-group form-control" name="groupCode" placeholder="Group Code">
                         </div>
 
-                        <div class="col-md-3">
-                            <input type="text" class="filter-group form-control" placeholder="Province">
+                        <div class="col-md-2">
+                            <select id="selectSite" name="selectSite" class="form-control">
+                                <option selected value="">SELECT ALL SITE</option>
+                                <option value="wpc2040">WPC2040</option>
+                                <option value="wpc2040aa">WPC2040AA</option>
+                            </select>
                         </div>
 
                         <div class="col-md-2">
                             <select id="selectType" name="selectType" class="form-control">
                                 <option selected value="">SELECT ALL TYPE</option>
-                                <option value="1">ARENA</option>
-                                <option value="2">OCBS-LOTTO</option>
-                                <option value="3">OCBS-OTB</option>
-                                <option value="4">OCBS-RESTOBAR</option>
-                                <option value="5">OCBS-STORE</option>
-                                <option value="6">OCBS-MALL</option>
-                                <option value="7">OCBS</option>
-                                <option value="8">OCBS-EGAMES</option>
-                                <option value="9">OCBS-CASINO</option>
+                                <option value="ARENA">ARENA</option>
+                                <option value="OCBS-LOTTO">OCBS-LOTTO</option>
+                                <option value="OCBS-OTB">OCBS-OTB</option>
+                                <option value="OCBS-RESTOBAR">OCBS-RESTOBAR</option>
+                                <option value="OCBS-STORE">OCBS-STORE</option>
+                                <option value="OCBS-MALL">OCBS-MALL</option>
+                                <option value="OCBS">OCBS</option>
+                                <option value="OCBS-EGAMES">OCBS-EGAMES</option>
+                                <option value="OCBS-CASINO">OCBS-CASINO</option>
                             </select>
                         </div>
 
-                        <!-- <div class="col-md-2">
-                            <select id="siteID" name="siteID" class="form-control">
-                                <option selected disabled value="">SELECT SITE</option>
-                                <option value="wpc2040">WPC2040</option>
-                                <option value="wpc2040aa">WPC2040AA</option>
-                            </select>
-                        </div> -->
-
                         <div class="col">
-                            <button type="button" class="btn btn-success"><i class="fas fa-search"></i> Submit</button>
+                            <button type="submit" class="btn btn-success"><i class="fas fa-search"></i> Submit</button>
+                            <a href="{{ url('/schedules/view') }}/{{ $scheduleId }}" class="btn btn-danger">Reset</a>
                         </div>
                         </div>
                     </form>
 
                     @include('schedules.tables.fullview')
+                    
+                    <div class="col">
+                        <div class="float-right">
+                            {{ $scheduledGroups->links('pagination::bootstrap-4') }}
+                        </div>
+                    </div>
 
                 </div>
 
