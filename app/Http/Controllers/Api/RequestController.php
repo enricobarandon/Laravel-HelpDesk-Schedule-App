a175a42220962fb0b765d09400cc4159e2f46527
Enrico <enricobarandon@gmail.com>
2022-06-22 00:24:13 +0800

    ocbs process trigger realtime

--- app/Http/Controllers/Api/RequestController.php
+++ app/Http/Controllers/Api/RequestController.php
@@ -301,8 +301,7 @@ class RequestController extends Controller
 
         } else {
             // update request
-            ProcessRequest::dispatch();
-            
+
             $acceptChanges = RequestModel::where('uuid', $request->uuid)
                                 ->where('operation', $request->operation)
                                 ->where('status', 'pending')
@@ -310,6 +309,8 @@ class RequestController extends Controller
                                     'status' => $request->status,
                                     // 'remarks' => DB::raw('remarks') . ' ; ' . $request->remarks
                                 ]);
+                
+            ProcessRequest::dispatch();
                                 
             if ($acceptChanges && $request->status == 'approved') {
 
