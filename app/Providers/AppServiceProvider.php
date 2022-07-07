deba4892f72541dfebd5d32877e29c558e1b4581
Enrico <enricobarandon@gmail.com>
2022-07-07 22:19:33 +0800

    development env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('APP_ENV') == 'production') {
+        if (env('APP_ENV') == 'production' || env('APP_ENV') == 'development') {
 
             URL::forceScheme('https');
 
