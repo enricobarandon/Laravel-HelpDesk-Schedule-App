8a7cf6310aaa244cf0e8cd9b408e6506ab1a541d
samSAM <samuelbandola@gmail.com>
2022-07-13 17:12:30 +0800

    download users with status

--- resources/views/users/tables/usersTable.blade.php
+++ resources/views/users/tables/usersTable.blade.php
@@ -6,6 +6,7 @@
             <th>Email</th>
             <th>User Role</th>
             <th>Created At</th>
+            <th>Status</th>
         </tr>
     </thead>
     <tbody>
@@ -19,6 +20,13 @@
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->role }}</td>
                 <td>{{ date("M d, Y h:i:s a",strtotime($user->created_at)) }}</td>
+                <td>
+                    @if($user->is_active)
+                        Active
+                    @else
+                        Deactivated
+                    @endif
+                </td>
             </tr>
         @endforeach
     </tbody>
