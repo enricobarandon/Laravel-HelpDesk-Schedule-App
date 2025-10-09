2dd5a2ae73bd292d1082adc7fe0f1c1c2fda6a7f
samSAM <samuelbandola@gmail.com>
2025-10-09 13:11:20 +0800

    update logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
                 <div class="container login-container">
                     <div class="row justify-content-center">
                         <div class="col-md-6 text-center justify-content-center align-items-center">
-                            <img src="dist/img/web-logo.jpg" alt="logo" class="header-mobile__logo-img logo-img  mb-2">
+                            <img src="dist/img/manila_arena.png" alt="logo" class="header-mobile__logo-img logo-img  mb-2">
                         </div>
                         <div class="col-md-6">
                             <div class="card card-login">
